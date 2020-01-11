d52ebf6ab748d1e5c32ec97ff14da4304b8be504
QrnaJnyynpr <d.wallace@live.co.uk>
2020-01-11 18:04:38 +0000

    Improved readability

---
+++ crew_data.py
@@ -0,0 +1,56 @@
+crew_data = [ # Manually updated
+{
+	"name": "Christina H. Koch",
+	"age": 40,
+	"country": "USA",
+	"photo": "koch.jpg",
+	"wiki": "https://en.wikipedia.org/wiki/Christina_Koch",
+	"bio": "Christina Hammock Koch, (born January 29, 1979) is an American engineer and NASA astronaut of the class of 2013. She received Bachelor of Science degrees in Electrical Engineering and Physics, and a Master of Science in Electrical Engineering from North Carolina State University. She also did advanced study while working for the Goddard Space Flight Center. Just before becoming an astronaut, she served with the National Oceanic and Atmospheric Administration, as Station Chief for American Samoa. On March 14, 2019, she launched to the International Space Station as a Flight Engineer on Expedition 59, 60 and 61. On October 18, 2019, Koch and Jessica Meir were the first women to participate in an all-female spacewalk.",
+	"alt": "Portrait photo of Christina Koch. Wearing a blue NASA jumpsuit adorned with insignias."
+},
+{
+	"name": "Alexander Skvortsov",
+	"age": 52,
+	"country": "Russia",
+	"photo": "skvortsov.jpg",
+	"wiki": "https://en.wikipedia.org/wiki/Aleksandr_Skvortsov_(cosmonaut)",
+	"bio": "Aleksandr Aleksandrovich Skvortsov (Russian: Aлександр Aлександрович Скворцов; born May 6, 1966) is a Russian cosmonaut. He has had two spaceflights, which were long-duration missions aboard the International Space Station. His first spaceflight took place from April to September 2010, and was launched with the spacecraft Soyuz TMA-18. He arrived at the station part way through Expedition 23, of which Oleg Kotov was the Commander. When Expedition 24 began in June, Skvortsov became Commander.",
+	"alt": "Portrait photo of Alexander Skvortsov. Wearing a full white space suit adorned with insignias."
+},
+{
+	"name": "Luca Parmitano",
+	"age": 43,
+	"country": "Italy",
+	"photo": "parmitano.jpg",
+	"wiki": "https://en.wikipedia.org/wiki/Luca_Parmitano",
+	"bio": "Luca Parmitano (born 27 September 1976 in Paternò, Sicily) is an Italian engineer and astronaut in the European Astronaut Corps for the European Space Agency (ESA). The astronauts work on missions at the International Space Station. He was selected as an ESA astronaut in May 2009. Parmitano is also a Colonel and pilot for the Italian Air Force. Parmitano is the youngest astronaut to undertake a long-duration mission, at 36 years and eight months old on the launch day of his mission.",
+	"alt": "Portrait photo of Luca Parmitano. Wearing a full white space suit adorned with insignias."
+},
+{
+	"name": "Andrew R. Morgan",
+	"age": 43,
+	"country": "USA",
+	"photo": "morgan.jpg",
+	"wiki": "https://en.wikipedia.org/wiki/Andrew_R._Morgan",
+	"bio": "Andrew Richard \"Drew\" Morgan (born February 5, 1976) is a NASA astronaut from the class of 2013. Morgan is a colonel in the U.S. Army from New Castle, Pennsylvania. Morgan is a graduate of the U.S. Military Academy at West Point, and earned a doctorate in medicine from the Uniformed Services University of the Health Sciences, Bethesda, Maryland. He has experience as an emergency physician and flight surgeon for the Army special operations community, and completed a sports medicine fellowship through Virginia Commonwealth University in Fairfax, Virginia. He made his first flight into space in July 2019 on board Soyuz MS-13, joining the ISS Expedition 60/61 crew as a flight engineer. In November 2019, he cast a ballot from outer space in that year's Pennsylvania elections. He and his wife, Stacey, have four children.",
+	"alt": "Portrait photo of Andrew Morgan. Wearing a blue NASA jumpsuit adorned with insignias."
+},
+{
+	"name": "Oleg Skripochka",
+	"age": 49,
+	"country": "Russia",
+	"photo": "skripochka.jpg",
+	"wiki": "https://en.wikipedia.org/wiki/Oleg_Skripochka",
+	"bio": "Oleg Ivanovich Skripochka (Russian: Оле́г Ива́нович Скри́почка; born 24 December 1969 in Nevinnomyssk, Stavropol Krai, Russian SFSR, Soviet Union) is a Russian cosmonaut. In 2011 he was in space serving as an Expedition 25/26 crewmember. On 12 April 2011, Skripochka was awarded the titles of Hero of the Russian Federation and Pilot-Cosmonaut of the Russian Federation for courage and heroism in the implementation of long-duration space flight on the International Space Station.",
+	"alt": "Portrait photo of Oleg Skripochka. Wearing a full white space suit adorned with insignias."
+},
+{
+	"name": "Jessica Meir",
+	"age": 42,
+	"country": "USA",
+	"photo": "meir.jpg",
+	"wiki": "https://en.wikipedia.org/wiki/Jessica_Meir",
+	"bio": "Jessica Ulrika Meir (IPA: /mɪər/; m-eer; born July 1, 1977) is a NASA astronaut, marine biologist, and physiologist. She was previously Assistant Professor of Anesthesia at Harvard Medical School, Massachusetts General Hospital, Boston, following postdoctoral research in comparative physiology at the University of British Columbia. She has studied the diving physiology and behavior of emperor penguins in Antarctica, and the physiology of bar-headed geese, which are able to migrate over the Himalayas. In September 2002, Meir served as an aquanaut on the NASA Extreme Environment Mission Operations 4 (NEEMO 4) crew. In 2013 she was selected by NASA to Astronaut Group 21. Meir launched on September 25, 2019, to the ISS onboard Soyuz MS-15, where she will serve as a flight Engineer during Expedition 61 and 62. On October 18, 2019, Meir and Christina Koch were the first women to participate in an all-female spacewalk.",
+	"alt": "Portrait photo of Jessica Meir. Wearing a full white space suit adorned with insignias."
+}
+]
